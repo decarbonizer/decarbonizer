aa60c18ceb60073a53eb8ee679f2ee69b3f31e4f
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-11-03 11:38:08 +0100

    Enable basic connection to the backend.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
+import { getMessage } from './api/message';
 import * as styles from './App.css';
 
 export default function App() {
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    getMessage().then((res) => setMessage(res.data.content));
+  }, []);
+
   return (
     <div className={styles.appContainer}>
       <h1>👋🏻 Hello from the frontend!</h1>
+      {message && (
+        <>
+          <br />
+          <b>Message from the backend:</b>
+          <code>{message}</code>
+        </>
+      )}
     </div>
   );
 }
