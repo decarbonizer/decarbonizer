ce20759cf1c45ab3fa32efc71b8e86d543fda09e
n.duong <nhu.duong@tum.de>
2021-11-27 19:08:21 +0100

    temp

--- packages/frontend/src/pages/survey/SurveyAnswerDrawer.tsx
+++ packages/frontend/src/pages/survey/SurveyAnswerDrawer.tsx
@@ -26,7 +26,12 @@ import {
 import { SurveyAnswer } from '../../api/surveyAnswer';
 import { FaEdit } from 'react-icons/fa';
 import { MdDeleteForever } from 'react-icons/md';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useDeleteSurveyAnswerMutation } from '../../store/api';
+import { useParams, useHistory } from 'react-router';
+import { Survey } from '../../api/survey';
+import { SurveysPageParams, routes } from '../../routes';
+import SurveyView from './SurveyView';
 
 export interface SurveyAnswerDrawer {
   isOpen: boolean;
@@ -36,6 +41,20 @@ export interface SurveyAnswerDrawer {
 
 export function SurveyAnswerDrawer({ isOpen, onClose, surveyAnswers }: SurveyAnswerDrawer) {
   const { isOpen: isOpenAlert, onOpen: onOpenAlert, onClose: onCloseAlert } = useDisclosure();
+  const [deleteSurveyAnswerMutation, { isLoading }] = useDeleteSurveyAnswerMutation();
+  const onConfirm = (surveyAnswer) => {
+    deleteSurveyAnswerMutation({ id: surveyAnswer._id });
+  };
+
+  const [activeSurvey, setActiveSurvey] = useState<Survey | undefined>(undefined);
+  const { realEstateId } = useParams<SurveysPageParams>();
+  const history = useHistory();
+
+  const finishSurvey = () => {
+    setActiveSurvey(undefined);
+    history.push(routes.realEstateDashboard({ realEstateId }));
+  };
+
   return (
     <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="sm">
       <DrawerOverlay />
@@ -53,12 +72,16 @@ export function SurveyAnswerDrawer({ isOpen, onClose, surveyAnswers }: SurveyAns
                     <Text>{(surveyAnswer.value as any).realEstateName}</Text>
                     <Spacer />
                     <Tooltip label="Edit" hasArrow>
-                      <IconButton aria-label="edit" icon={<FaEdit />} mr="1" />
+                      <IconButton aria-label="edit" icon={<FaEdit />} mr="1" onClick={} />
                     </Tooltip>
                     <Tooltip label="Delete" hasArrow>
                       <IconButton aria-label="delete" fontSize="21" icon={<MdDeleteForever />} onClick={onOpenAlert} />
                     </Tooltip>
-                    <DeleteAlertDialog isOpen={isOpenAlert} onClose={onCloseAlert} />
+                    <DeleteAlertDialog
+                      isOpen={isOpenAlert}
+                      onCancel={onCloseAlert}
+                      onConfirm={() => onConfirm(surveyAnswer)}
+                    />
                   </Td>
                 </Tr>
               ))}
@@ -66,30 +89,37 @@ export function SurveyAnswerDrawer({ isOpen, onClose, surveyAnswers }: SurveyAns
           </Table>
         </DrawerBody>
       </DrawerContent>
+      <Drawer placement="bottom" size="full" isOpen={!!activeSurvey} onClose={null!}>
+        <DrawerOverlay />
+        <DrawerContent>
+          {activeSurvey && <SurveyView realEstateId={realEstateId} surveyId={activeSurvey._id} onDone={finishSurvey} />}
+        </DrawerContent>
+      </Drawer>
     </Drawer>
   );
 }
 
 interface DeleteAlertDialogProps {
   isOpen: boolean;
-  onClose: () => void;
+  onCancel: () => void;
+  onConfirm: () => void;
 }
 
-function DeleteAlertDialog({ isOpen, onClose }: DeleteAlertDialogProps) {
+function DeleteAlertDialog({ isOpen, onCancel, onConfirm }: DeleteAlertDialogProps) {
   const cancelRef = useRef<any>();
 
   return (
-    <AlertDialog motionPreset="slideInBottom" leastDestructiveRef={cancelRef} onClose={onClose} isOpen={isOpen}>
+    <AlertDialog motionPreset="slideInBottom" leastDestructiveRef={cancelRef} onClose={onCancel} isOpen={isOpen}>
       <AlertDialogOverlay />
       <AlertDialogContent>
         <AlertDialogHeader>Delete survey answer?</AlertDialogHeader>
         <AlertDialogCloseButton />
         <AlertDialogBody>Are you sure you want to delete your survey answer?</AlertDialogBody>
         <AlertDialogFooter>
-          <Button ref={cancelRef} onClick={onClose}>
+          <Button ref={cancelRef} onClick={onCancel}>
             No, keep it.
           </Button>
-          <Button colorScheme="red" ml={3}>
+          <Button colorScheme="red" ml={3} onClick={onConfirm}>
             Yes, delete it.
           </Button>
         </AlertDialogFooter>
