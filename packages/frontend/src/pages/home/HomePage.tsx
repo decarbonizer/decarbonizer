e94fe91175b2653fac0eb8b1e7bcbe980c34d4df
n.duong <nhu.duong@tum.de>
2021-11-27 16:25:48 +0100

    Deleted test daterangepicker.

--- packages/frontend/src/pages/home/HomePage.tsx
+++ packages/frontend/src/pages/home/HomePage.tsx
@@ -1,19 +1,10 @@
-import { useState } from 'react';
 import DefaultPageLayout from '../../components/DefaultPageLayout';
-import TimeRangeInput from '../../components/TimeRangInput';
 import RealEstateBox from './RealEstateBox';
 
 export default function HomePage() {
-  const [blub, setBlub] = useState<any>();
   return (
     <DefaultPageLayout title="Home">
       <RealEstateBox />
-      <TimeRangeInput
-        value={blub}
-        onValueChanged={(e) => {
-          setBlub(e);
-        }}
-      />
     </DefaultPageLayout>
   );
 }
