906bd3e40f07cf6a1f1018a2c01a11cb825e4aad
n.duong <nhu.duong@tum.de>
2021-11-30 13:14:32 +0100

    deleted tests.

--- packages/frontend/src/pages/home/HomePage.tsx
+++ packages/frontend/src/pages/home/HomePage.tsx
@@ -1,18 +1,10 @@
 import DefaultPageLayout from '../../components/DefaultPageLayout';
-import SortingSelection from '../../components/SortingSelection';
 import RealEstateBox from './RealEstateBox';
 
 export default function HomePage() {
   return (
     <DefaultPageLayout title="Home">
       <RealEstateBox />
-      <SortingSelection
-        sortingCategories={[
-          { value: 'test1', display: 'test1' },
-          { value: 'test2', display: 'test2' },
-        ]}
-        onChange={(e) => console.log(e)}
-      />
     </DefaultPageLayout>
   );
 }
