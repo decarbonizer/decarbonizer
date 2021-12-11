121e12131518cbaa2c2433528fe82045f9146607
n.duong <nhu.duong@tum.de>
2021-12-11 12:47:34 +0100

    .

--- packages/frontend/src/data/actions/it/itActions.ts
+++ packages/frontend/src/data/actions/it/itActions.ts
@@ -3,6 +3,7 @@ import { RiDatabaseLine } from 'react-icons/all';
 import { increaseDataCenterTemperature } from './increaseDataCenterTemperature';
 
 export const itActions: ActionCategory = {
+  id: 'it',
   name: 'IT',
   icon: RiDatabaseLine,
   actions: [increaseDataCenterTemperature],
