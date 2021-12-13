a26eae8c7b2b014218d7e2bfa645c2a2e5efcd1a
Vanessa Krohn <vanessa.krohn@tum.de>
2021-12-14 00:23:57 +0100

    Added year comparison to electricity chart section.

--- packages/frontend/src/pages/dashboard/electricity/ElectricityChartSection.tsx
+++ packages/frontend/src/pages/dashboard/electricity/ElectricityChartSection.tsx
@@ -1,12 +1,14 @@
 import { Grid } from '@chakra-ui/react';
 import FootprintDeltaCard from './FootprintDeltaCard';
 import CostDeltaCard from './CostDeltaCard';
+import ComparisonOfCostsAndFootprints from './CostFootprintComparisonCard';
 
 export default function ElectricityChartsSection() {
   return (
     <Grid flexGrow={1} templateColumns="repeat(6, 1fr)" templateRows="auto 1fr" gap="6">
       <FootprintDeltaCard gridRow="1" gridColumn="1 / span 2" />
       <CostDeltaCard gridRow="1" gridColumn="3 / span 2" />
+      <ComparisonOfCostsAndFootprints gridRow="2 / span 13" gridColumn="1 / span 3" />
     </Grid>
   );
 }
