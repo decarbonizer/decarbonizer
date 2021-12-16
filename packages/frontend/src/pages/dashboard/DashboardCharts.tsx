c167edbeedffadde61ce88f92da4ae29da35afec
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-12-16 16:05:33 +0100

    Generally enable comparison.

--- packages/frontend/src/pages/dashboard/DashboardCharts.tsx
+++ packages/frontend/src/pages/dashboard/DashboardCharts.tsx
@@ -1,13 +1,13 @@
-import { Box, Flex, Select, Skeleton } from '@chakra-ui/react';
+import { Box, Flex, Select, SimpleGrid, Skeleton } from '@chakra-ui/react';
 import { useContext, useState } from 'react';
 import { useParams } from 'react-router';
 import { ActionPlan } from '../../api/actionPlan';
-import { ActionCategory } from '../../data/actions/action';
+import { ActionCategory, KnownActionId } from '../../data/actions/action';
 import { RealEstatePageParams } from '../../routes';
 import { useGetAllActionPlansForRealEstateQuery } from '../../store/api';
 import ActionChartsSection from './ActionChartsSection';
 import ChartSectionHeader from './components/ChartSectionHeader';
-import { DashboardContext } from './dashboardContext';
+import { DashboardContext, FilledActionAnswers } from './dashboardContext';
 import GlobalChartsSection from './GlobalChartsSection';
 
 export interface DashboardChartsProps {
@@ -16,13 +16,18 @@ export interface DashboardChartsProps {
 
 export default function DashboardCharts({ selectedActionCategory }: DashboardChartsProps) {
   const { realEstateId } = useParams<RealEstatePageParams>();
-  const { actionPlanToEdit } = useContext(DashboardContext);
+  const dashboardContext = useContext(DashboardContext);
+  const { actionPlanToEdit } = dashboardContext;
   const { data: actionPlans, isLoading: isLoadingActionPlans } = useGetAllActionPlansForRealEstateQuery({
     realEstateId,
   });
   const comparableActionPlans = (actionPlans ?? []).filter((actionPlan) => actionPlan._id !== actionPlanToEdit?._id);
   const [actionPlanComparand, setActionPlanComparand] = useState<ActionPlan | undefined>(undefined);
   const isComparingPlans = !!actionPlanComparand;
+  const actionPlanComparandAnswers = (actionPlanComparand?.actionAnswers ?? []).reduce<FilledActionAnswers>(
+    (acc, actionAnswer) => ({ ...acc, [actionAnswer._id as KnownActionId]: actionAnswer }),
+    {},
+  );
 
   return (
     <>
@@ -49,7 +54,16 @@ export default function DashboardCharts({ selectedActionCategory }: DashboardCha
           </Select>
         )}
       </Flex>
-      <GlobalChartsSection isNarrow={isComparingPlans} />
+
+      <SimpleGrid columns={isComparingPlans ? 2 : 1} gap={8}>
+        <GlobalChartsSection isNarrow={isComparingPlans} />
+        {isComparingPlans && (
+          <DashboardContext.Provider value={{ ...dashboardContext, filledActionAnswers: actionPlanComparandAnswers }}>
+            <GlobalChartsSection isNarrow={isComparingPlans} />
+          </DashboardContext.Provider>
+        )}
+      </SimpleGrid>
+
       {selectedActionCategory && (
         <>
           <ChartSectionHeader
@@ -58,9 +72,17 @@ export default function DashboardCharts({ selectedActionCategory }: DashboardCha
             mt="8"
             mb="4"
           />
-          <Box flexGrow={1} mb="8" display="flex" alignItems="stretch">
-            <ActionChartsSection isNarrow={isComparingPlans} />
-          </Box>
+          <SimpleGrid columns={isComparingPlans ? 2 : 1} gap={8}>
+            <Box flexGrow={1} mb="8" display="flex" alignItems="stretch">
+              <ActionChartsSection isNarrow={isComparingPlans} />
+            </Box>
+            {isComparingPlans && (
+              <DashboardContext.Provider
+                value={{ ...dashboardContext, filledActionAnswers: actionPlanComparandAnswers }}>
+                <ActionChartsSection isNarrow={isComparingPlans} />
+              </DashboardContext.Provider>
+            )}
+          </SimpleGrid>
         </>
       )}
     </>
