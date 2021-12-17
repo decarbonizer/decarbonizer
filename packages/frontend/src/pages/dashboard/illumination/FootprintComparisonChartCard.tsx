ad90a65ed5b6d0ec67e0d736aca478c1b74f7495
Vanessa Krohn <vanessa.krohn@tum.de>
2021-12-17 22:25:30 +0100

    Added minimum height to comparisons charts.

--- packages/frontend/src/pages/dashboard/illumination/FootprintComparisonChartCard.tsx
+++ packages/frontend/src/pages/dashboard/illumination/FootprintComparisonChartCard.tsx
@@ -1,15 +1,13 @@
-import { SkeletonText } from '@chakra-ui/react';
-import { CartesianGrid, XAxis, YAxis, Tooltip, Area, AreaChart, Legend, ResponsiveContainer } from 'recharts';
 import {
   getIlluminationFootprintPerYear,
   getTransformedIlluminationFootprintPerYear,
 } from '../../../calculations/illumination/footprint';
 import { useCalculation } from '../../../calculations/useCalculation';
-import InlineErrorDisplay from '../../../components/InlineErrorDisplay';
 import { useFilledActionAnswersDataFrame } from '../dashboardContext';
-import DashboardCard, { DashboardCardProps } from '../components/DashboardCard';
+import { DashboardCardProps } from '../components/DashboardCard';
 import range from 'lodash-es/range';
 import { getSurveyAnswersForSurvey } from '../../../calculations/surveyAnswers/getSurveyAnswersForSurvey';
+import ComparisonChartCard from '../components/ComparisonChartCard';
 
 export default function FootprintComparisonChartCard(props: DashboardCardProps) {
   const filledActionAnswersDf = useFilledActionAnswersDataFrame();
@@ -39,31 +37,15 @@ export default function FootprintComparisonChartCard(props: DashboardCardProps) 
   );
 
   return (
-    <DashboardCard header="Footprint comparison over 10 years" isExpandable {...props}>
-      <InlineErrorDisplay error={error}>
-        {isLoading && <SkeletonText />}
-        {data && (
-          <ResponsiveContainer width="100%" height="100%">
-            <AreaChart
-              data={data}
-              syncId="illuminationFootprintComparison"
-              margin={{
-                top: 10,
-                right: 30,
-                left: 0,
-                bottom: 15,
-              }}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="Year" label={{ value: 'Years', position: 'insideBottomRight', offset: -10 }} />
-              <YAxis />
-              <Tooltip />
-              <Legend margin={{ top: 10 }} offset={-10} />
-              <Area type="monotone" dataKey="New footprint" stroke="#9AE6B4" strokeWidth={3} fill="#9AE6B477" />
-              <Area type="monotone" dataKey="Old footprint" stroke="#B794F4" strokeWidth={3} fill="#B794F477" />
-            </AreaChart>
-          </ResponsiveContainer>
-        )}
-      </InlineErrorDisplay>
-    </DashboardCard>
+    <ComparisonChartCard
+      {...props}
+      header="Footprint comparison over 10 years"
+      data={data}
+      isLoading={isLoading}
+      error={error}
+      syncId="illuminationFootprintComparison"
+      oldDataKey="Old footprint"
+      newDataKey="New footprint"
+    />
   );
 }
