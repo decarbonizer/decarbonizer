aead76a10361f4d9759f5dea05720b17ba10f7cc
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-01-17 16:51:40 +0100

    Added card layout to budget page.

--- packages/frontend/src/pages/budget/BudgetPage.tsx
+++ packages/frontend/src/pages/budget/BudgetPage.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@chakra-ui/react';
+import Card from '../../components/Card';
 import DefaultPageLayout from '../../components/DefaultPageLayout';
 import ActionPlanChart from './ActionPlanChart';
 import BudgetChart from './BudgetChart';
@@ -8,11 +9,29 @@ export default function BudgetPage() {
   const toYear = 2050;
 
   return (
-    <DefaultPageLayout title="Budget Planning">
-      <Grid w="100%" h="100%" templateRows="10% 1fr">
-        <ActionPlanChart fromYear={fromYear} toYear={toYear} />
-        <BudgetChart fromYear={fromYear} toYear={toYear} />
-      </Grid>
+    <DefaultPageLayout
+      contentProps={{ pr: '0' }}
+      leftArea={
+        <Card
+          as="aside"
+          isStatic
+          flexGrow={1}
+          w="md"
+          borderBottomRadius={0}
+          borderLeftRadius={0}
+          h="calc(100vh - 5rem)"
+          px="8"
+          py="4"
+          size="lg">
+          Foo
+        </Card>
+      }>
+      <Card w="100%" h="100%" pr="8" py="4" borderBottomRadius={0} borderTopRightRadius={0} isStatic>
+        <Grid w="100%" h="100%" templateRows="10% 1fr">
+          <ActionPlanChart fromYear={fromYear} toYear={toYear} />
+          <BudgetChart fromYear={fromYear} toYear={toYear} />
+        </Grid>
+      </Card>
     </DefaultPageLayout>
   );
 }
