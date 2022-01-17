4e144e96e995992836c541ded9ff3724a4001157
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-01-17 17:52:43 +0100

    Readd bar.

--- packages/frontend/src/pages/budget/BudgetChart.tsx
+++ packages/frontend/src/pages/budget/BudgetChart.tsx
@@ -30,7 +30,7 @@ export default function BudgetChart({ fromYear, toYear, ...rest }: BudgetChartPr
           <YAxis width={100} domain={['data-min', 'data-max']} unit="€" scale="linear" />
           <Bar dataKey="totalOriginalCost" name="Cost Without Action Plans" fill="#bAf6d4" unit="€" />
           <Bar dataKey="totalNewCost" name="Cost With Action Plans" fill="#9AE6B4" unit="€" />
-          <Bar dataKey="delta" name="Cost Delta" opacity={0.6} fill="gray" unit="€" hide />
+          <Bar dataKey="delta" name="Cost Delta" opacity={0.6} fill="gray" unit="€" />
           <ReferenceLine y={0} stroke="black" />
         </BarChart>
       </ResponsiveContainer>
