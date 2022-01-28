af1a84ac0664cde8ff11e21edbd2ddeb068cf4ab
Alexandra Talalaieva <25621530+sashatalalasha@users.noreply.github.com>
2022-01-28 18:29:54 +0100

    add message about compensated heating

--- packages/frontend/src/pages/dashboard/heating/CalculatedCostsCard.tsx
+++ packages/frontend/src/pages/dashboard/heating/CalculatedCostsCard.tsx
@@ -1,4 +1,4 @@
-import { SkeletonText, Table, Tbody, Td, Tr } from '@chakra-ui/react';
+import { SkeletonText, Table, Tbody, Td, Tr, Text } from '@chakra-ui/react';
 import { useCalculation } from '../../../calculations/useCalculation';
 import InlineErrorDisplay from '../../../components/InlineErrorDisplay';
 import { useFilledActionAnswersDataFrame } from '../dashboardContext';
@@ -36,32 +36,39 @@ export default function CalculatedCostsCard(props: DashboardCardProps) {
       <InlineErrorDisplay error={error}>
         {isLoading && <SkeletonText noOfLines={6} spacing="4" />}
         {data && (
-          <Table variant="">
-            <Tbody>
-              <Tr>
-                <Td fontWeight="bold" fontSize="lg" pl="0">
-                  {data.heatingCosts.toFixed(2)}€
-                </Td>
-                <Td>Heating costs per year</Td>
-              </Tr>
-              <Tr>
-                <Td fontWeight="bold" fontSize="lg" pl="0">
-                  {data.footprint.toFixed(2)}t
-                </Td>
-                <Td>Carbon emissions through heating</Td>
-              </Tr>
-              {data.producedHeating > 0 ? (
+          <>
+            <Table variant="">
+              <Tbody>
                 <Tr>
                   <Td fontWeight="bold" fontSize="lg" pl="0">
-                    {data.producedHeating.toFixed(2)}kWh
+                    {data.heatingCosts.toFixed(2)}€
                   </Td>
-                  <Td>Heating compensated by data center</Td>
+                  <Td>Heating costs per year</Td>
                 </Tr>
-              ) : (
-                <></>
-              )}
-            </Tbody>
-          </Table>
+                <Tr>
+                  <Td fontWeight="bold" fontSize="lg" pl="0">
+                    {data.footprint.toFixed(2)}t
+                  </Td>
+                  <Td>Carbon emissions through heating</Td>
+                </Tr>
+                {data.producedHeating > 0 ? (
+                  <Tr>
+                    <Td fontWeight="bold" fontSize="lg" pl="0">
+                      {data.producedHeating.toFixed(2)}kWh
+                    </Td>
+                    <Td>Heating compensated by data center</Td>
+                  </Tr>
+                ) : (
+                  <></>
+                )}
+              </Tbody>
+            </Table>
+            {parseInt(data.heatingCosts.toFixed(2)) === 0 && parseInt(data.footprint.toFixed(2)) === 0 && (
+              <Text color="green">
+                Produced heating by data center completely compensates current heating costs and footprint
+              </Text>
+            )}
+          </>
         )}
       </InlineErrorDisplay>
     </DashboardCard>
