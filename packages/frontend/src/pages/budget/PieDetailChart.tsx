0bfeacefae2820baff65e76b35b0a67ebb0fb542
n.duong <nhu.duong@tum.de>
2022-01-31 13:15:28 +0100

    Added custom tooltip to pie detail chart.

--- packages/frontend/src/pages/budget/PieDetailChart.tsx
+++ packages/frontend/src/pages/budget/PieDetailChart.tsx
@@ -4,16 +4,15 @@ import undraw_savings_re_eq4w from '../../img/undraw_savings_re_eq4w.svg';
 import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
 import { categories } from '../../calculations/calculations/getBudgetChartData';
 import { DeltaResult } from '../../utils/deltaType';
+import { DefaultTooltipContent } from 'recharts/lib/component/DefaultTooltipContent';
 
 export interface CostChartDataEntry {
   name: string;
   cost: number;
-  percent: string;
 }
 export interface SavingChartDataEntry {
   name: string;
   saving: number;
-  percent: string;
 }
 
 export interface PieDetailChartProps {
@@ -28,58 +27,53 @@ export default function PieDetailChart({ investmentCosts, originalCosts, profit 
   const dataCosts: Array<CostChartDataEntry> = [];
   const dataSavings: Array<SavingChartDataEntry> = [];
 
+  const CustomTooltip = ({ payload }: any) => {
+    if (payload?.length) {
+      const percent = Math.round((payload[0].value / Math.abs(profit)) * 100);
+      return <DefaultTooltipContent payload={[...payload, { name: 'Percentage', value: `${percent}%` }]} />;
+    } else {
+      return null;
+    }
+  };
+
   for (let i = 0; i < investmentCosts.length; i++) {
     const cost = Math.round(investmentCosts[i] + originalCosts[i].delta);
-    const percent = (Math.abs(cost) / Math.abs(profit)) * 100;
     if (cost < 0) {
       dataSavings.push({
         name: categories[i],
         saving: Math.abs(cost),
-        percent: `${percent}%`,
       });
       dataCosts.push({
         name: categories[i],
         cost: 0,
-        percent: '0%',
       });
     } else {
       dataSavings.push({
         name: categories[i],
         saving: 0,
-        percent: '0%',
       });
       dataCosts.push({
         name: categories[i],
         cost: Math.abs(cost),
-        percent: `${percent}%`,
       });
     }
   }
   const newDataCosts = dataCosts.filter((category) => category.cost > 0);
   const newDataSavings = dataSavings.filter((category) => category.saving > 0);
 
-  console.log(newDataCosts);
-  console.log(newDataSavings);
   return (
     <HStack>
       <Box w="50%" h="md">
-        <Heading size="md">Costs</Heading>
+        <Heading size="md">Costs in €</Heading>
         {newDataCosts.length ? (
           <ResponsiveContainer width="100%" height="100%">
             <PieChart>
-              <Pie
-                data={newDataCosts}
-                dataKey="cost"
-                innerRadius={80}
-                outerRadius={130}
-                fill="#8884d8"
-                paddingAngle={3}
-                label>
+              <Pie data={newDataCosts} dataKey="cost" innerRadius={80} outerRadius={130} fill="#8884d8" label>
                 {newDataCosts.map((entry, index) => (
                   <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
               </Pie>
-              <Tooltip />
+              <Tooltip content={<CustomTooltip />} />
             </PieChart>
           </ResponsiveContainer>
         ) : (
@@ -91,23 +85,16 @@ export default function PieDetailChart({ investmentCosts, originalCosts, profit 
         )}
       </Box>
       <Box w="50%" h="md">
-        <Heading size="md">Savings</Heading>
+        <Heading size="md">Savings in €</Heading>
         {newDataSavings.length ? (
           <ResponsiveContainer width="100%" height="100%">
             <PieChart>
-              <Pie
-                data={newDataSavings}
-                dataKey="saving"
-                innerRadius={80}
-                outerRadius={130}
-                fill="#8884d8"
-                paddingAngle={3}
-                label>
+              <Pie data={newDataSavings} dataKey="saving" innerRadius={80} outerRadius={130} fill="#8884d8" label>
                 {newDataSavings.map((entry, index) => (
                   <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
               </Pie>
-              <Tooltip />
+              <Tooltip content={<CustomTooltip />} />
             </PieChart>
           </ResponsiveContainer>
         ) : (
