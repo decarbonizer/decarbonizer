284676429564ac9aaea57fbf232bbd16ea974c58
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-02-05 17:25:17 +0100

    Removed comments.

--- packages/frontend/src/calculations/calculations/getGlobalRealEstateFootprintCardData.ts
+++ packages/frontend/src/calculations/calculations/getGlobalRealEstateFootprintCardData.ts
@@ -1,44 +1,8 @@
-// const { isLoading, data, error } = useCalculation(
-//   (externalCalculationData) => {
-//     const surveyAnswers = externalCalculationData.surveyAnswers.filter(
-//       (surveyAnswer) => surveyAnswer.realEstateId === realEstateId && surveyAnswer.value.isInitialSurvey,
-//     );
-//     const actionPlansRealEstate = externalCalculationData.actionPlans.filter(
-//       (actionPlan) => actionPlan.realEstateId === realEstateId,
-//     );
-//     const originalFootprint = getGlobalSummedYearlyFootprint(externalCalculationData, surveyAnswers);
-
 import { DataFrame } from 'data-forge';
 import { ActionAnswerBase } from '../../api/actionAnswer';
 import { SurveyAnswer } from '../../api/surveyAnswer';
 import { ExternalCalculationData } from '../useExternalCalculationData';
 import { getGlobalSummedYearlyFootprint } from './getGlobalSummedYearlyFootprint';
-
-//     const actionPlanFootprints = actionPlansRealEstate
-//       .map((actionPlan) => {
-//         const footprintActionPlan = getGlobalSummedYearlyFootprintDelta(
-//           externalCalculationData,
-//           surveyAnswers,
-//           new DataFrame(actionPlan.actionAnswers),
-//         ).delta;
-//         return footprintActionPlan;
-//       })
-//       .reduce((a, b) => a + b, 0);
-
-//     const footprintAction = getGlobalSummedYearlyFootprintDelta(
-//       externalCalculationData,
-//       surveyAnswers,
-//       filledActionAnswersDf,
-//     ).delta;
-
-//     const overallFootprint = originalFootprint + footprintAction + actionPlanFootprints;
-
-//     return {
-//       overallFootprint,
-//     };
-//   },
-//   [filledActionAnswersDf],
-// );
 
 export function getGlobalRealEstateFootprintCardData(
   externalCalculationData: ExternalCalculationData,
