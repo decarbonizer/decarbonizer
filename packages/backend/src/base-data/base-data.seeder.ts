8602da494c714b1db9d2bf4d23aa534c2ec5a5ba
Alexandra Talalaieva <25621530+sashatalalasha@users.noreply.github.com>
2022-02-05 13:10:17 +0100

    add seed base data for city hamburg

--- packages/backend/src/base-data/base-data.seeder.ts
+++ packages/backend/src/base-data/base-data.seeder.ts
@@ -27,6 +27,23 @@ export class BaseDataSeeder extends DefaultDbSeeder<BaseData> {
         longTravelEF: 0.19309,
         illuminationEF: 0.624,
       },
+      {
+        _id: '00000000-0000-0000-0000-000000000002',
+        illuminationEF: 0.624,
+        longTravelEF: 0.19309,
+        shortTravelEF: 0.15353,
+        heatingKwHPerQm: 0.1,
+        footPrintServer: 320,
+        serverLifeTime: 8,
+        serverMaintenanceTime: 6,
+        normalServerCost: 1200,
+        superServerCost: 2000,
+        reductionFactorByUsingSuperServer: 0.3,
+        salaryItMaintenanceWorkerPerHour: 100,
+        timeToChangeOneBulb: 10,
+        salaryElectricianMaintenanceWorkerPerHour: 125,
+        realEstateId: '00000000-0000-0000-0000-000000000002',
+      },
     ];
   }
 }
