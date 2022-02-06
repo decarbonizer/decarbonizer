ee6b30cec937863de9787e9bed76a16761ae5eb5
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2022-02-06 15:16:47 +0100

    Updated base data seeding for hamburg.

--- packages/backend/src/base-data/base-data.seeder.ts
+++ packages/backend/src/base-data/base-data.seeder.ts
@@ -12,22 +12,6 @@ export class BaseDataSeeder extends DefaultDbSeeder<BaseData> {
   protected override getSeed(): Array<BaseData> {
     return [
       {
-        realEstateId: '00000000-0000-0000-0000-000000000002',
-        salaryElectricianMaintenanceWorkerPerHour: 12, // €/h
-        timeToChangeOneBulb: 10, // min/Stk
-        salaryItMaintenanceWorkerPerHour: 100, // €/h
-        reductionFactorByUsingSuperServer: 0.3,
-        superServerCost: 2000,
-        normalServerCost: 1200,
-        serverMaintenanceTime: 6, // h/stk
-        serverLifeTime: 8, //years
-        footPrintServer: 320, //Co2
-        heatingKwHPerQm: 0.1,
-        shortTravelEF: 0.15353,
-        longTravelEF: 0.19309,
-        illuminationEF: 0.624,
-      },
-      {
         _id: '00000000-0000-0000-0000-000000000002',
         illuminationEF: 0.624,
         longTravelEF: 0.19309,
@@ -41,7 +25,7 @@ export class BaseDataSeeder extends DefaultDbSeeder<BaseData> {
         reductionFactorByUsingSuperServer: 0.3,
         salaryItMaintenanceWorkerPerHour: 100,
         timeToChangeOneBulb: 10,
-        salaryElectricianMaintenanceWorkerPerHour: 125,
+        salaryElectricianMaintenanceWorkerPerHour: 12,
         realEstateId: '00000000-0000-0000-0000-000000000002',
       },
     ];
